23f85d7ea15f6e371e9f09ff539f87461f8fc3dd
Danil Kovtonyuk <danx.kov@gmail.com>
2021-08-17 13:30:46 +1000

    feat: add xDai

--- mustache/templates/proxy/contracts.js
+++ mustache/templates/proxy/contracts.js
@@ -6,6 +6,12 @@ const contracts = [
     address: '0x0D5550d52428E7e3175bfc9550207e4ad3859b17',
   },
   {
+    prod: 17626717,
+    name: 'Proxy',
+    network: 'xdai',
+    address: '0xa77350E1a72bE5bC7634d359C5011b4E359f4320',
+  },
+  {
     prod: 16257989,
     name: 'Proxy',
     network: 'matic',
